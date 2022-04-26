7ed3f7875cadd50e7cdd9b3b01f64cda24288b71
ann <dorisann.me@gmail.com>
2022-04-26 17:57:33 +0000

    tweaked the arrangements a bit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function getWeather(location) {
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=d6bab5e0b374c1fb0431c40a61afa691`,
+    `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=d6bab5e0b374c1fb0431c40a61afa691`,
     { mode: "cors" }
   );
   if (!response.ok) alert(`City ${location} not found`);
@@ -10,20 +10,20 @@ async function getWeather(location) {
   console.log(sys, "sys");
   console.log(weather, "weatherdata");
 
-  displayData(main, sys, weather, location);
+  displayData(location, sys, main, weather);
 }
 
-function displayData(main, sys, weather, location) {
+function displayData(location, sys, main, weather) {
   const cityName = document.getElementById("city-name");
+  const temperature = document.getElementById("temperature");
   const feelsLike = document.getElementById("feels-like");
   const humidity = document.getElementById("humidity");
-  const temperature = document.getElementById("temperature");
   const weatherDescription = document.getElementById("description");
 
   cityName.textContent = `${location}, ${sys.country}`;
+  temperature.textContent = `${main.temp}°C`;
   feelsLike.textContent = `It feels like ${main.feels_like}°C`;
   humidity.textContent = `Humidity: ${main.humidity}%`;
-  temperature.textContent = `Temperature: ${main.temp}°C`;
   weatherDescription.textContent = `Description: ${weather[0].description}`;
 }
 
